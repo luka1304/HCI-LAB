c61c053234ecc7833cf599e409aaf9dc4d9a72a6
luka1304 <lmrduljas1304@gmail.com>
2021-10-26 20:37:33 +0200

    Lab2

---
+++ lmrduljas/Lab2/next-course-app/pages/home.js
@@ -0,0 +1,11 @@
+//import React from "react"
+
+import Header from "../modules/header/header";
+
+//function Home() {
+//    return <h1>Hello home</h1>;
+//}
+
+const Home = () => <Header />
+
+export default Home;
